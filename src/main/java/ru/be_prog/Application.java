e3d570bccd97d3d6285e6cafa01d05f776d02aaf
bogdanov.andrey37 <bogdanov.andrey37@wb.ru>
2024-12-23 20:27:22 +0300

    Java database initial commit

---
+++ src/main/java/ru/be_prog/Application.java
@@ -0,0 +1,14 @@
+package ru.be_prog;
+
+
+import ru.be_prog.service.AccountService;
+import ru.be_prog.service.AccountServiceImpl;
+
+public class Application {
+
+    public static void main(String[] args) {
+        AccountService accountService = new AccountServiceImpl();
+
+        // здесь должен располагаться алгоритм из задачи
+    }
+}
---
+++ src/main/java/ru/be_prog/dao/AccountDaoJdbcImpl.java
@@ -0,0 +1,54 @@
+package ru.be_prog.dao;
+
+import ru.be_prog.Account;
+import ru.be_prog.util.DatabaseJdbcUtil;
+
+import java.util.List;
+import java.util.UUID;
+
+public class AccountDaoJdbcImpl implements AccountDao {
+
+    private static final DatabaseJdbcUtil DATABASE_JDBC_UTIL = new DatabaseJdbcUtil();
+
+    @Override
+    public void createAccountTable() {
+        // Реализуем создание таблицы здесь
+    }
+
+    @Override
+    public void dropAccountTable() {
+        // Реализуем удаление таблицы здесь
+    }
+
+    @Override
+    public void deleteAllAccounts() {
+        // Реализуем удаление всех Account
+    }
+
+    @Override
+    public void createAccount(Account account) {
+        // Реализуем создание нового Account
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        // Реализуем обновление Account
+    }
+
+    @Override
+    public void deleteAccount(UUID id) {
+        // Реализуем удаление Account по id
+    }
+
+    @Override
+    public Account findAccount(UUID id) {
+        // Реализуем получение Account по id
+        return null;
+    }
+
+    @Override
+    public List<Account> findAllAccounts() {
+        // Реализуем получение всех Account
+        return List.of();
+    }
+}
